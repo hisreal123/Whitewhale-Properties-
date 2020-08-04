e961b3b140fabe5d733df7c425f25e0f8f259ce4
George Isreal <omosgoerge@gmail.com>
2020-08-04 04:19:18 +0100

    WhiteWhale Properties

---
+++ js/main.js
@@ -0,0 +1,93 @@
+$(document).ready(function () {
+
+    let tpNav = $('.tpNav');
+    let logo = $('.logo');
+    let wrap = $('.wrap')
+    let smallWtihLogo = $('.wthLogo')
+    let banner = $('.banner')
+    let imgS = $(".imgBox");
+
+    // adding shadow  to all the imgs when the page finish loading 
+
+    $(".boxImg").addClass('shadow');
+
+    $(window).scroll(function () {
+        let wScroll = $(this).scrollTop();
+
+
+        if(wScroll > 100) {
+          tpNav.addClass('fixed-top')
+        }
+    });
+
+
+
+
+
+    // away from scroll
+    // Showing h1.logo
+    logo.delay().addClass('in_animation')
+    smallWtihLogo.delay().addClass('in_animation')
+
+    $('.banner').owlCarousel({
+      autoplay: true,
+      loop: true,
+      items: 1,
+      autoWidth: false,
+      autoHeight: false, 
+      nav: false, 
+      dots: false,
+      smartSpeed: 600,
+    })
+
+    // owl-carousel
+    $(".showCase_grid").owlCarousel({
+      // autoHeight: false ,
+      // autoWidth: false ,
+      margin: 30,
+      smartSpeed: 1000,
+      loop: true,
+      items: 1,
+      autoplay: true,
+
+      responsive: {
+        0: {
+          items: 1,
+        },
+
+        400: {
+          items: 2,
+        },
+
+        600: {
+          items: 3,
+        },
+
+        1200: {
+          items: 4,
+        },
+      },
+    });
+// displaying the SERVICE note
+
+$(".d-note").on('click', function(){
+  $(".floatContent").fadeIn();
+  $(".oLay").fadeIn();
+  });
+
+
+$('.close').on('click', function(){
+  $(".floatContent").fadeOut(200);
+  $(".oLay").fadeOut();
+});
+
+
+// setBg function
+  $('.bannerChildWrap').each(function(){
+    let bg = $(this).data('setbg');
+    $(this).css({
+      'background-image': 'url(' + bg + ')'
+    });
+  })
+
+})
